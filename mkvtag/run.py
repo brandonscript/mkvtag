7fde0f2f9ea0b964d6b49dd6f6bf74eef8944882
Brandon Shelley <brandon@pacificaviator.co>
2024-06-26 14:25:34 -0700

    Change to json-based status keeper and actively track state

--- mkvtag/run.py
+++ mkvtag/run.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import subprocess
 import sys
@@ -6,144 +7,271 @@ import time
 from collections.abc import Sequence
 from datetime import datetime
 from pathlib import Path
+from typing import Literal
 
+from deepdiff import DeepDiff
 from humanize import naturaltime
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
 SLEEP_TIME = 5 if "pytest" in sys.modules else 30
+MODIFIED_SAFE_TIME = 30
+LOG_FILE_NAME = "mkvtag.json"
+
+Status = Literal["new", "done", "processing", "waiting", "failed", "gone"]
 
 
 class File:
-    def __init__(self, file_path: Path):
+    def __init__(self, file_path: Path, tagger: "MkvTagger"):
         self.path = file_path
-        self.mtime = self.get_mtime()
-        self.size = self.get_size()
-        self._processed = False
+        self._last_mtime = 0.0
+        self._mtime = 0.0
+        self._last_size = 0
+        self._size = 0
+        self._status: Status = "new"
+        self._tagger = tagger
 
     def __repr__(self):
-        return f"File({self.name}, {self.friendly_mtime}, {self.friendly_size}, done={self.processed})"
+        return f"File({self.name}, {self.friendly_mtime}, {self.friendly_size}, status={self.status})"
 
     @property
     def name(self):
         return self.path.name
 
     @property
+    def mtime(self):
+        try:
+            if self._mtime != self._last_mtime:
+                self._last_mtime = self._mtime
+            self._mtime = Path(self.path).stat().st_mtime
+            return self._mtime
+        except FileNotFoundError:
+            return 0.0
+
+    @property
     def friendly_mtime(self):
         """Use humanized time format from 'humanize' package"""
 
-        return naturaltime(datetime.now() - datetime.fromtimestamp(self.mtime))
+        return naturaltime(datetime.now() - datetime.fromtimestamp(self._mtime))
 
-    def get_mtime(self):
+    @property
+    def was_recently_modified(self):
+        return time.time() - self.mtime < MODIFIED_SAFE_TIME
+
+    @property
+    def size(self):
         try:
-            return Path(self.path).stat().st_mtime
+            if self._size != self._last_size:
+                self._last_size = self._size
+            self._size = Path(self.path).stat().st_size
+            return self._size
         except FileNotFoundError:
-            return 0.0
+            return 0
 
     @property
     def friendly_size(self):
         """Use humanized size format from 'humanize' package"""
         from humanize import naturalsize
 
-        return naturalsize(self.size or 0)
-
-    def get_size(self):
-        try:
-            return Path(self.path).stat().st_size
-        except FileNotFoundError:
-            return 0
+        return naturalsize(self._size or 0)
 
     @property
-    def processed(self):
-        if self._processed:
-            return True
-        self._processed = is_processed(self, self.path.parent)
-        return self._processed
+    def status(self) -> Status:
+        if self._status:
+            return self._status
+        self._tagger.scan()
+        if not (ref := self._tagger.files.get(self.name)):
+            self._tagger.set_status(self, "gone")
+            self._status = "gone"
+            return self._status
+
+        self._status = ref.status
+        return self._status
 
     @classmethod
-    def from_csv(cls, line: str, watch_dir: Path):
-        name, mtime, size = line.strip().split(",")
-        new = cls(watch_dir / name)
-        new.mtime = float(mtime)
-        new.size = int(size)
+    def from_json(cls, data: dict, *, tagger: "MkvTagger"):
+        new = cls(tagger.watch_dir / data["name"], tagger)
+        mtime = data["mtime"]
+        # convert from ISO 8601 format to timestamp
+        try:
+            mtime = datetime.fromisoformat(mtime).timestamp()
+        except ValueError:
+            ...
+        new._last_mtime = mtime
+        new._mtime = mtime
+        new._last_size = data["size"]
+        new._size = data["size"]
+        new._status = data["status"]
         return new
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "mtime": datetime.fromtimestamp(self.mtime).isoformat(),
+            "size": self.size,
+            "status": self._status,
+        }
+
+    def save_to_log(self, log_file: Path, *, update_status: Status | None = None):
+        if update_status:
+            self._status = update_status
+        known_files = self._tagger.scan()
+        known_files[self.name] = self
+        with open(log_file, "w") as f:
+            # update the log file if the file already exists, otherwise append
+            json.dump({f.name: f.to_json() for f in known_files.values()}, f, indent=4)
 
     def __eq__(self, other):
         return self.name == other.name
 
 
 class MkvTagger(FileSystemEventHandler):
-    current_files: dict[str, File]
-    processed_files: dict[str, File]
-    _active = False
+    files: dict[str, File]
+    _is_processing = False
+    _active_file: str | None = None
 
     def __init__(self, watch_dir: Path = Path.cwd()):
         self.watch_dir = watch_dir
-        self.log_file = self.watch_dir / "processed_files.txt"
+        self.log_file = self.watch_dir / LOG_FILE_NAME
+        self.files = {}
 
-        if self.log_file.exists():
-            self.log_file.unlink()
+        if not self.watch_dir.exists():
+            raise FileNotFoundError(f"Watch dir '{self.watch_dir}' does not exist")
+
+        if not self.log_file.exists():
             self.log_file.touch()
 
         print("Watching for new files...\n")
 
-        self.scan()
-        # print(self.current_files, self.processed_files)
+        self.files = self.scan(reset=True)
 
         self.process_dir()
 
-    def scan(self):
-        self.current_files = scan_watch_dir(self.watch_dir)
-        self.processed_files = load_processed_files(self.watch_dir)
-
     def process_dir(self):
-        for file in self.current_files.values():
-            if not self.is_file_done(file) and self.is_file_ready(file):
-                self.process_file(file)
+        for file in self.files.values():
+            self.process_file(file)
 
     def on_modified(self, event):
+
+        if LOG_FILE_NAME in event.src_path:
+            # update the files list if the log file is modified
+
+            # if log file matches self.files, skip
+            if not DeepDiff(self.files, self.scan(), ignore_order=True):
+                return
+
+            self.files.update(self.scan())
+
         if event.src_path.endswith(".mkv"):
             # print(f"File '{event.src_path}' has been modified.")
             # print(self.current_files, self.processed_files)
             # self.scan()
-            file = File(Path(event.src_path))
-            if not self.is_file_done(file) and self.is_file_ready(file):
-                self.process_file(file)
+            file = File(Path(event.src_path), self)
+            self.process_file(file)
 
-    def is_file_done(self, file: File):
-        current_size = file.get_size()
-        if (
-            (mkv := self.processed_files.get(file.name, None))
-            and mkv.processed
-            and mkv.size == current_size
-        ):
-            return True
+    def is_active_file(self, file: File):
+        return file.name == self._active_file
 
-        return False
+    def set_status(self, file: File, status: Status):
+        # if file is not in self.files, add it
+        if file.name not in self.files:
+            self.files[file.name] = file
+        file._status = status
+        self.files[file.name]._status = status
+        file.save_to_log(self.log_file, update_status=status)
 
-    def is_file_ready(self, file: File):
+    def scan(self, reset: bool = False) -> dict[str, "File"]:
+        log_file = self.watch_dir / LOG_FILE_NAME
+        logged_files = {}
+        dir_files = {f.name: File(f, self) for f in Path(self.watch_dir).glob("*.mkv")}
+        if not log_file.exists():
+            # write {} to log file
+            with open(log_file, "w") as f:
+                json.dump({}, f, indent=4)
+        with open(log_file, "r") as f:
+            # Step 2: Load JSON data
+            try:
+                file_data = json.load(f)  # Assuming file_data is a list of dicts
+                # if file data is not an object, raise
+                if not isinstance(file_data, dict):
+                    raise json.JSONDecodeError(
+                        "Invalid JSON data, expected an object.", "", 0
+                    )
+            except json.JSONDecodeError:
+                print("Error decoding JSON data from log file, resetting log file.\n")
+                file_data = {}
+                # save empty list to log file
+                with open(log_file, "w") as f:
+                    json.dump({}, f, indent=4)
 
-        # if file is recently modified, skip
-        if file.get_mtime() > time.time() - SLEEP_TIME:
-            return False
+            # Step 3 & 4: Process each item in the list
+            logged_files = {
+                item["name"]: File.from_json(
+                    item, tagger=self
+                )  # Assuming a new method `from_json`
+                for item in file_data.values()
+            }
 
-        current_size = file.get_size()
-        if (
-            mkv := self.current_files.get(file.name, None)
-        ) and mkv.size == current_size:
-            return True
+            # Update the status of files that are no longer in the directory
+            for name, file in logged_files.items():
+                if name not in dir_files.keys():
+                    self.set_status(file, "gone")
 
-        # print(f"File '{file.name}' is not ready yet.")
+            # Add new files from dir_files to logged_files
+            new_files = {
+                name: file
+                for name, file in dir_files.items()
+                if name not in logged_files.keys()
+            }
 
-        return False
+            merged = {**logged_files, **new_files}
+
+            # If reset is true, reset the status on all files
+            # unless the status is "done" or "gone"
+            if reset:
+                for file in merged.values():
+                    if file.status not in ["done", "gone", "failed", "waiting"]:
+                        self.set_status(file, "new")
+
+            return merged
 
     def process_file(self, file: File):
 
-        if file.processed or self._active:
+        if file.name.startswith("28"):
+            ...
+
+        if file.status in ["done", "gone"]:
             return
 
+        if self._is_processing or self.is_active_file(file):
+            return
+
+        if file.was_recently_modified:
+            if file.status == "waiting":
+                return
+            self.set_status(file, "waiting")
+            time_since_modified_friendly = naturaltime(
+                datetime.now() - datetime.fromtimestamp(file.mtime)
+            )
+            if time_since_modified_friendly == "now":
+                time_since_modified_friendly = "a second ago"
+            print(
+                f"File '{file.name}' was modified {time_since_modified_friendly}, skipping for now...",
+                file.__dict__,
+            )
+            time.sleep(SLEEP_TIME)
+            if file.was_recently_modified:
+                self.set_status(file, "waiting")
+                return
+
+        if file.status == "failed":
+            print(f"File '{file.name}' failed to process, retrying...")
+            self.set_status(file, "new")
+
         print(f"Processing file: {file.name}")
-        self._active = True
+        self._is_processing = True
+        self._active_file = file.name
+        self.set_status(file, "processing")
 
         try:
             cmd: Sequence[str | Path] = [
@@ -162,63 +290,22 @@ class MkvTagger(FileSystemEventHandler):
                             stdout.write(line)
                             stdout.flush()
             else:
-                res = subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL)
+                res = subprocess.run(
+                    cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
                 if res.returncode != 0:
                     raise subprocess.CalledProcessError(res.returncode, res.args)
                 print(f"Done\n")
+                self.set_status(file, "done")
 
-            updated = self.current_files[file.name]
-            updated.mtime = file.get_mtime()
-            updated.size = file.get_size()
-            updated._processed = True
-            self.save_processed_files()
         except subprocess.CalledProcessError as e:
-            proc_error = e.stderr.decode("utf-8")
-            print(f"Error processing file {file.name}:\n{proc_error}")
-
-        self._active = False
-
-    def save_processed_files(self):
-        new_processed_files = {
-            mkv.name: mkv
-            for mkv in self.current_files.values()
-            if mkv.processed and not mkv.name in self.processed_files.keys()
-        }
-        with open(self.log_file, "a") as f:
-            for mkv in new_processed_files.values():
-                # skip if file is already in the log file
-                if mkv.name in load_processed_files(self.watch_dir).keys():
-                    continue
-                f.write(f"{mkv.name},{mkv.mtime},{mkv.size}\n")
-
-        self.processed_files = new_processed_files
-
-
-def scan_watch_dir(watch_dir: Path):
-    return {f.name: File(f) for f in Path(watch_dir).glob("*.mkv")}
-
-
-def load_processed_files(watch_dir: Path):
-    log_file = watch_dir / "processed_files.txt"
-    processed_files = {}
-    Path(log_file).touch(exist_ok=True)
-    with open(log_file, "r") as f:
-        processed_files = {
-            mkv.name: mkv
-            for mkv in [(File.from_csv(line, watch_dir)) for line in f]
-            if mkv.name in scan_watch_dir(watch_dir).keys()
-        }
-
-        # mark all processed files as done
-        for mkv in processed_files.values():
-            mkv._processed = True
-
-        return processed_files
-
-
-def is_processed(file: File, watch_dir: Path):
-    processed_files = load_processed_files(watch_dir)
-    return bool(processed_files.get(file.name, None))
+            proc_error = e.stderr.decode("utf-8") if e and e.stderr else ""
+            print(f"Error processing file {file.name}:")
+            print(proc_error)
+            self.set_status(file, "failed")
+        finally:
+            self._is_processing = False
+            self._active_file = None
 
 
 def main():
